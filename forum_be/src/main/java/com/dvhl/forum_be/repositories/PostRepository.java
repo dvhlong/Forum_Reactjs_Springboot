50b5efb6c7b472b3845e061b6ca2ecadf2dc4c0a
Long <long59915@gmail.com>
2022-05-13 07:20:19 +0700

    13/05

--- forum_be/src/main/java/com/dvhl/forum_be/repositories/PostRepository.java
+++ forum_be/src/main/java/com/dvhl/forum_be/repositories/PostRepository.java
@@ -7,7 +7,6 @@ import com.dvhl.forum_be.model.Post;
 import com.dvhl.forum_be.model.Topic;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
